ac410e47b02b13783e69bb36c1aa84fd5f01a67b
MSYK <jimmule8@gmail.com>
2022-02-21 20:10:51 +0300

    Landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,230 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
-@endsection
+@endsection --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ABIDAS</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<body>
+
+    <section id="header">
+        <a href="#">
+            <img src="/assets/images/img/logo.png" alt="logo">
+        </a>
+
+        <div>
+            <ul id="navbar">
+                <li><a class="active" href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/shop') }}">Shop</a></li>
+                <li><a href="{{ url('/blog') }}">Blog</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+                <li>
+                    <a href="{{ url('/cart') }}">
+                        <i class='bx bx-shopping-bag'></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="hero">
+        <h4>Trade-in-offer</h4>
+        <h2>Super Value Deals</h2>
+        <h1>On all Products</h1>
+        <p>Use coupons for up to 70% off</p>
+        <button onclick="location.href='{{ url('shop') }}'">Shop Now</button>
+    </section>
+
+    <section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f1.png" alt="">
+            <h6>Free shipping</h6>
+        </div>
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f2.png" alt="">
+            <h6>Online Order</h6>
+        </div>
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f3.png" alt="">
+            <h6>Save Money</h6>
+        </div>
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f4.png" alt="">
+            <h6>Promotions</h6>
+        </div>
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f5.png" alt="">
+            <h6>Happy Sell</h6>
+        </div>
+        <div class="fe-box">
+            <img src="/assets/images/img/features/f6.png" alt="">
+            <h6>24/7 Support</h6>
+        </div>
+    </section>
+
+    <section id="product1" class="section-p1">
+        <h2>Featured Products</h2>
+        <p>New Summer Collection</p>
+        <div class="pro-container">
+            <div class="pro">
+                <img src="/assets/images/img/products/f1.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f2.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f3.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f4.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f5.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f6.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f7.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+
+            <div class="pro">
+                <img src="/assets/images/img/products/f8.jpg" alt="">
+                <div class="des">
+                    <span>Adidas</span>
+                    <h5>Cartoon Astronaut T-shirt</h5>
+                    <div class="star">
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                        <i class='bx bxs-star'></i>
+                    </div>
+                    <h4>Ksh. 780</h4>
+                </div>
+                <a href=""><i class='bx bx-cart-alt cart'></i></a>
+            </div>
+        </div>
+    </section>
+
+    <section id="banner" class="section-m1">
+        <h4>Repair Services</h4>
+        <h2>Up to <span>70% off </span> on New Arrivals & Accessories</h2>
+        <button class="normal">Explore More</button>
+    </section>
+
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+
+</html>
