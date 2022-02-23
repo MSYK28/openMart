9f01382f5bbd7ff3b4399848bb002353f0b406a4
Tori-Stark <61185176+Tori-Stark@users.noreply.github.com>
2022-02-23 12:43:54 +0300

    Revert "display products"
    This reverts commit af0d8c906dd89b20ef31e252410a6273ad7e8bf4.

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -26,54 +26,138 @@ class ShoppingCartController extends Controller
         return view('shop.product',['product'=>$product]);
     }
 
-    public function add_to_cart(Items $product){
 
-       // dd($product);
+    public function add_to_cart($id){
 
-        //$product = Products::findOrFail($productid);
+        // dd($product);
+
+        $product = Items::findOrFail($id);
+        $cart = session()->get('cart', []);
 
         //add to cart
-        \Cart::session(auth()->id())->add(array(
-        'id' => $product->id,
-        'name' => $product->name,
-        //'category'=>$product->category,
-        'price' => $product->price,
-        'quantity' => 1,
-        'attributes' => array(),
-        'associatedModel' => $product
-    ));
-
-    return redirect()->route('cart.index');
-    }
-
-     public function cart(){
-
-        $cartItems = \Cart::session(auth()->id())->getContent();
-
-        return view('hiquip.cart',compact('cartItems'));
-    }
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name "=> $product->name,
+                //'category'=>$product->category,
+                "price" => $product->price,
+                'quantity' => 1,
+                "image" => $product->item_imge
+            ];
 
 
-     public function cart_destroy($itemId){
+            session()->put('cart', $cart);
+            return redirect()->back()->with('success', 'Product added to cart successfully!');
+        }
 
-        $cartItems = \Cart::session(auth()->id())->remove($itemId);
+        public function cart(){
 
-        return back();
-    }
-    public function cart_update($rowId){
+            return view('hiquip.cart');
+        }
 
-        \Cart::session(auth()->id())->update($rowId, ['quantity'=> array(
-                'relative' => false,
-                'value' => request('quantity')
-        )
-    ]);
+        public function cart_remove(){
 
-        return back();
-    }
-    public function checkout(){
+            if($request->id) {
+                $cart = session()->get('cart');
+                if(isset($cart[$request->id])) {
+                    unset($cart[$request->id]);
+                    session()->put('cart', $cart);
+                }
+                session()->flash('success', 'Product removed successfully');
+            }
+            public function cart_update(){
 
-        return view('cart.checkout');
-    }
+                if($request->id && $request->quantity){
+                    $cart = session()->get('cart');
+                    $cart[$request->id]["quantity"] = $request->quantity;
+                    session()->put('cart', $cart);
+                    session()->flash('success', 'Cart updated successfully');
+                }
+                ]);
+
+                return back();
+            }
+
+            public function cart_remove(){
+
+                if($request->id) {
+                    $cart = session()->get('cart');
+                    if(isset($cart[$request->id])) {
+                        unset($cart[$request->id]);
+                        session()->put('cart', $cart);
+                    }
+                    session()->flash('success', 'Product removed successfully');
+                }
+
+                public function checkout(){
+
+                    return view('cart.checkout');
+                }
+
+                public function checkout_order(Request $request){
+
+                    $cart = session()->get('cart');
+                    foreach(session('cart') as $id => $details){
+
+                        $total += $details['price'] * $details['quantity']
+
+      }
+                    $checkout = $this->validate($request,
+                        [
+                            'phone'=> 'required|regex:/(07)[0-9]{8}/',
+                            'location'=> 'required|max:255',
+                            'payment_method' =>'required'
+                        ]);
+
+                    $order = new Order();
 
 
-}
+                    $order->phone_number =$request->input('name');
+                    $order->location = $request->input('location');
+                    $order->payment_method = $request->input('payment_method');
+
+                    $order->grand_total = $total;
+                    $order->user_id = auth()->id();
+
+                    $order->save();
+
+                    //save order items
+
+                    foreach($cartItems as $item){
+
+                        $order->items()->attach($item->id, ['order_id'=>$order->id,'quantity'=> $item->quantity ]);
+                    }
+
+
+
+                    //payment
+                    /* if($request->input('payment_method') == 'mpesa'){
+                         return redirect()->route('mpesa.checkout');
+                     }*/
+                    //send email to customer
+                    if($order->save() == true){
+                        $recepient_email = $request->email;
+
+                        Mail::to($request->email)->send(new OrderMail($checkout));
+                        Session::flash('msg','Order successful');
+
+                    }else{
+                        echo "Error";
+
+                    }
+                    //empty cart
+                    \Cart::session(auth()->id())->clear();
+
+
+
+                    /*take user to thank you
+                    return "order completed,thak you for order";*/
+
+                    return redirect()->route('hiquip');
+
+                }
+
+
+
+            }
